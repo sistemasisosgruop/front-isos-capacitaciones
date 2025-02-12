1e3f169a618b4b67de6a9efed016bfd156362427
Cristhian Ocola <ocolapintoc@gmail.com>
2025-02-12 15:37:53 -0500

    Fix:email-optional

--- src/pages/administrador/trabajador/validateFormModal.js
+++ src/pages/administrador/trabajador/validateFormModal.js
@@ -7,7 +7,10 @@ const validate = () => {
       apellidoPaterno: [ value => value.length >= 1, 'Este campo es requerido' ],
       apellidoMaterno: [ value => value.length >= 1, 'Este campo es requerido' ],
       dni: [ value => regexs.dni.test(value), 'Este DNI no es valido' ],
-      email: [ value => regexs.email.test(value), 'Este Email no es valido' ],
+      email: [ 
+        value => value === "" || regexs.email.test(value), // Si está vacío, pasa la validación.
+        'Este Email no es valido' 
+      ],
       genero: [ value => value.length >= 1, 'Este campo es requerido' ],
       edad: [ value => regexs.number.test(value), 'Este campo es requerido' ],
       areadetrabajo: [ value => value.length >= 1, 'Este campo es requerido' ],
